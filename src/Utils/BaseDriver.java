d37bbe70711dbbec545fea8baeb01a841220b5d7
kemalyayil <kemalyayil@gmail.com>
2021-08-09 13:51:40 -0300

    Base Driver and Reusable Methods

---
+++ src/Utils/BaseDriver.java
@@ -0,0 +1,65 @@
+package Utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseDriver {
+
+    // Webdriver driver - Access Modifier:
+    // should be public or protected
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\zeynep\\Google Drive\\Selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //bunu halit hoca ile beraber yaptik. zira AddressBookFunction 'da State "New York" secerken hata veriyordu.
+        // Hata nin country secerken refresh yapilmasi esnasinda selenium un hizli calisip
+        // united states i secememesinden kaynaklandigini anlayip, bu wait i buraya koyduk.
+        // sonrasinda kendi kodunda bulunan for loop u da comment out yaptik.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://opencart.abstracta.us/index.php?route=common/home");
+
+        WebElement myAccountButton = driver.findElement(By.xpath("//a[@title='My Account']"));
+        myAccountButton.click();
+
+        WebElement loginButton = driver.findElement(By.linkText("Login"));
+        loginButton.click();
+
+        WebElement advancedButton = driver.findElement(By.xpath("//button[@id='details-button']"));
+        advancedButton.click();
+
+        WebElement proceedLink = driver.findElement(By.xpath("//a[@id='proceed-link']"));
+        proceedLink.click();
+
+        WebElement username = driver.findElement(By.id("input-email"));
+        username.sendKeys("kk@kk.com");
+
+        WebElement password = driver.findElement(By.id("input-password"));
+        password.sendKeys("11111");
+
+        WebElement submitButton = driver.findElement(By.xpath("//input[@type='submit']"));
+        submitButton.click();
+
+        WebElement homePageButton = driver.findElement(By.xpath("//a[text()='Your Store']"));
+        homePageButton.click();
+    }
+
+    @AfterMethod
+    public void quit(){
+        driver.quit();
+    }
+
+}
+
+
+
---
+++ src/Utils/ReusableMethods.java
@@ -0,0 +1,45 @@
+package Utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+// This method will convert WebElement list into String List and return the String List.
+    public class ReusableMethods extends BaseDriver {
+    public static List<String> webElementListToStringList(List<WebElement> webElementList) {
+        List<String> convertedList = new ArrayList<>();
+
+        for (WebElement element : webElementList) {
+            convertedList.add(element.getText());
+        }
+        return convertedList;
+    }
+
+    // This method will compare two string List and will fail if there is assertion error.
+    public static void validateTwoStringList(List<String> actualItems, List<String> expectedItems) {
+        for (int i = 0; i < expectedItems.size(); i++) {
+            Assert.assertEquals(actualItems.get(i), expectedItems.get(i));
+        }
+
+    }
+
+//    public void verifySuccessMessage(WebDriver driver) { hocanin kodu
+
+//        WebElement successMessage = driver.findElement  (By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+//
+//        Assert.assertTrue(successMessage.getText().toLowerCase().contains("success"));
+
+    public  void verifySuccessMessage(WebDriver driver){
+        // success mesajlarinin icerikleri farkli oldugu icin sadece 'success' i lowercase yaparak aratip, pass
+        // veya fail olup olmadigiina bakiyoruz
+        WebElement successMessage = driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+        Assert.assertTrue(successMessage.getText().toLowerCase().contains("success"));
+    }
+
+    }
+
